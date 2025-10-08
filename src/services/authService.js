36d2bd7eb6f828b207f2e208d06e02b2d0481b2c
veeru2005 <sunkavalli.veerendra1973@gmail.com>
2025-10-08 16:32:04 +0530

    Fix API URLs to use environment variable instead of hardcoded localhost

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:30083/back1/auth";
+const API_URL = `${process.env.REACT_APP_BACKEND_URL}/back1/auth`;
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
